4432df8621a9028631483890d17e866430b9e728
Lee <lhwilli4@cougarnet.uh.edu>
2025-12-30 19:22:44 -0600

    fixed log gen

--- generate_data/generate_obs_table.py
+++ generate_data/generate_obs_table.py
@@ -3,7 +3,7 @@ import uuid
 import os
 doc_template = 'schemas/document_instance.json'
 '''
-template and degrading mode will be randomly selected from what is available
+template and omitting mode will be randomly selected from what is available
 '''
 debug_patient_path = "/home/leeha/Projects/EHR-processing/patients/0aaa2164-8de6-4152-8674-14d254aae13a/patient.json"
 with open(debug_patient_path) as f:
@@ -19,8 +19,8 @@ def select_template(type = None):
     }
     return OBS_TABLE_V1
 
-def select_degrade_mode():
-    return "PLACEHOLDER_DEGRADE_MODE"
+def select_omit_mode():
+    return "PLACEHOLDER_OMIT_MODE"
 
 def select_obs(patient=None, template = "obs_v1"):
     obs = [] #store obs entities by id
@@ -29,91 +29,114 @@ def select_obs(patient=None, template = "obs_v1"):
     for entity_type in template["entity_type"]:
         print(entity_type)
         for observation in patient[entity_type]: #iterate thru each observation entity
-            obs.append(observation["id"]) #build list of entity ids to include
+            obs.append(observation) #build list of entity ids to include
     return obs
 
 def omit_entities(obs = [], mode = None):
     if mode is None:
         return obs
     else:
-        pass
+        return obs
 
-def build_and_log_docs(patient = None, entities = None, template = None, degrading_mode = None):
-    dates = {} #create dict of dates so we can make doc for each one
-    documents = []
-    for obs_entity in patient["observations"]:
-        if obs_entity["date"] not in dates: 
-            dates[obs_entity["date"]] = [obs_entity]
+def group_obs(observations = None, grouping = "date"):
+    entity_groups = {} #each key need be a grouping value (e.g. a date or encounterID), each value need be a list of entities
+    for obs in observations: #observations == [] of obs entities
+        if obs[grouping] not in entity_groups:
+            entity_groups[obs[grouping]] = [obs]
         else:
-            dates[obs_entity["date"]].append(obs_entity)
-    print(dates.keys())
-    for date, entities in dates.items(): #make doc for each encounter
-        expected = []
-        expected_ids = []
-        realized = []
-        realized_ids= []
-        document = {
-            "doc_id": f"doc_{uuid.uuid1()}",
-            "doc_category": "obs_table",
-            "template_id": template["template_id"],
-            "patient_id": patient["patient"]["id"],
-            "date": date,
-            "entities_provided": {
-                "observations": [
-                    # {
-                    #     "entity_id": obs.id,
-                    #     "fields": ["description", "value", "units"]
-                    # }
-                    # for obs in realized_obs
-                ]
-            }
+            entity_groups[obs[grouping]].append(obs)
+    print(entity_groups.keys())
+    return entity_groups
+
+def build_and_log_doc(patient = None, date = None, expected_obs = None, realized_obs = None, template = None, omitting_mode = None):
+    # dates = {} #create dict of dates so we can make doc for each one
+    # documents = []
+    # for obs_entity in patient["observations"]:
+    #     if obs_entity["date"] not in dates: 
+    #         dates[obs_entity["date"]] = [obs_entity]
+    #     else:
+    #         dates[obs_entity["date"]].append(obs_entity)
+    # print(dates.keys())
+    # for date, entities in dates.items(): #make doc for each encounter
+    #     expected = []
+    #     expected_ids = []
+    #     realized = []
+    #     realized_ids= []
+    document = {
+        "doc_id": f"doc_{uuid.uuid4()}",
+        "doc_category": "obs_table",
+        "template_id": template["template_id"],
+        "patient_id": patient["patient"]["id"],
+        "date": date,
+        "entities_provided": {
+            "observations": [
+                {
+                    "entity_id": obs["id"],
+                    "fields": template["fields"]
+                }
+                for obs in realized_obs
+            ]
         }
-        for entity in entities: #add entity corresponding to this encounter to doc
-            # document["entities_provided"]["observations"].append(entity)
-            expected.append(entity)
-            expected_ids.append(entity["id"])
-        realized = omit_entities(expected, degrading_mode)
-        for entity in realized:
-            document["entities_provided"]["observations"].append(entity)
-            realized_ids.append(entity["id"])
-        with open(f"documents/{document["doc_id"]}", "w") as file: #create doc
-            json.dump(document, file, indent = 2)
-        documents.append(document)
-        for document in documents:
-            log = {
-            "log_id": f"log_{uuid.uuid1()}",
-            "patient_id" : patient["patient"]["id"],
-            "doc_id" : document["doc_id"],
-            "template_id" : document["template_id"],
-            "expected" : expected_ids,
-            "realized" : realized_ids,
-            "degrading_mode" : degrading_mode
-            }
-            with open(f"logs/{log["log_id"]}", "w") as file:
-                json.dump(log, file, indent = 2)
+    }
+    # for entity in entities: #add entity corresponding to this encounter to doc
+    #     # document["entities_provided"]["observations"].append(entity)
+    #     expected.append(entity)
+    #     expected_ids.append(entity["id"])
+    # realized = omit_entities(expected, omitting_mode)
+    # for entity in realized:
+    #     document["entities_provided"]["observations"].append(entity)
+    #     realized_ids.append(entity["id"])
+    # with open(f"documents/{document["doc_id"]}", "w") as file: #create doc
+    #     json.dump(document, file, indent = 2)
+    # documents.append(document)
+    #now create a log
+    # log_doc(document, patient, expected_ids, realized_ids, omitting_mode)
 
-    return documents
-# def log_docs(documents = None, patient = None, expected_obs = None, realized_obs = None, degrading_mode = None):
-#     for document in documents:
-#         log = {
-#             "log_id": f"log_{uuid.uuid1()}",
-#             "patient_id" : patient["patient"]["id"],
-#             "doc_id" : document["doc_id"],
-#             "template_id" : document["template_id"],
-#             "expected" : expected_obs,
-#             "realized" : realized_obs,
-#             "degrading_mode" : degrading_mode
+    log = {
+        "log_id": f"log_{uuid.uuid4()}",
+        "patient_id" : patient["patient"]["id"],
+        "doc_id" : document["doc_id"],
+        "template_id" : document["template_id"],
+        "expected" : [obs["id"] for obs in expected_obs],
+        "realized" : [obs["id"] for obs in realized_obs],
+        "omitting_mode" : omitting_mode
+    }
 
-#         }
-#         with open(f"logs/{log["log_id"]}", "w") as file:
-#             json.dump(log, file, indent = 2)
+    return document, log
+# def log_doc(document = None, patient = None, expected_obs = None, realized_obs = None, omitting_mode = None):
+    # for document in documents:
+    log = {
+        "log_id": f"log_{uuid.uuid4()}",
+        "patient_id" : patient["patient"]["id"],
+        "doc_id" : document["doc_id"],
+        "template_id" : document["template_id"],
+        "expected" : expected_obs,
+        "realized" : realized_obs,
+        "omitting_mode" : omitting_mode
+    }
+    with open(f"logs/{log["log_id"]}", "w") as file:
+        json.dump(log, file, indent = 2)
+def write_doc(document):
+    with open(f"documents/{document['doc_id']}", "w") as file:
+        json.dump(document, file, indent=2) 
+
+def write_log(log):
+    with open(f"logs/{log['log_id']}", "w") as file:
+        json.dump(log, file, indent = 2)
 
 def generate_observation_table(patient): #patient will be a json style dict
     template = select_template("observations")
-    degrade_mode = select_degrade_mode()
+    omit_mode = select_omit_mode()
     expected_obs = select_obs(patient, template)
-    realized_obs = omit_entities(obs = expected_obs, mode = degrade_mode)
-    docs = build_and_log_docs(patient = patient, entities = realized_obs, template = template)
-    # log_docs(docs, patient, expected_obs, realized_obs, degrade_mode)
+    grouped_obs = group_obs(expected_obs, grouping="date")
+    documents = []
+    for date, obs_for_date in grouped_obs.items():
+        realized_obs = omit_entities(obs_for_date, omit_mode)
+        document, log = build_and_log_doc(patient, date, obs_for_date, realized_obs, template, omit_mode)
+        write_doc(document)
+        write_log(log)
+        documents.append(document)
+
+    return documents
 
 generate_observation_table(debug_patient)
